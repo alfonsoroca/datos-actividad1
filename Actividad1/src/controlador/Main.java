1354fc343d84179bc55634f853aa22af4813f092
alfonsoroca <alfonsoroca@gmail.com>
2021-12-28 14:49:01 +0100

    Mejoras en el código.

--- Actividad1/src/controlador/Main.java
+++ Actividad1/src/controlador/Main.java
@@ -11,37 +11,34 @@ import modelo.Almacen;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Almacen almacen = null;
-		// Gestión del fichero coches.dat		
-		File fichero = new File ("coches.dat");
+		// Gestión del fichero coches.dat
+		File fichero = new File("coches.dat");
 
-		// Lógica que controla la creación o importación de datos al almacén.
+		System.out.println("Iniciando aplicación....");
+		System.out.println("Creando almacén....");
+		System.out.println("Almacén creado.");
+		almacen = new Almacen();
 
-		// Si el fichero no existe, creamos un almacen.
-		if (!fichero.exists()) {
-			System.out.println("No existe ningún almacén creado....");
-			System.out.println("	Creando almacén nuevo....");
-			System.out.println("Almacén creado.");
-			almacen = new Almacen();
-			
-		// Si el fichero existe, importamos sus datos y los pasamos al almacen.	
-		} else {
+		// Lógica que importa los datos al almacén cuando existe el fichero coches.dat.
+		if (fichero.exists()) {
 
-			try (FileInputStream fis = new FileInputStream(fichero); ObjectInputStream ois = new ObjectInputStream(fis)){
-				
-				System.out.println("Importando datos del almacén.....");
+			try (FileInputStream fis = new FileInputStream(fichero);
+					ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+				System.out.println("Importando datos al almacén.....");
 				almacen = (Almacen) ois.readObject();
-				
-			} catch (FileNotFoundException e) {				
+
+			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
-			}			
-		}		
-		
+			}
+		}
+
 		// Asociamos el almacén al menu.
 		Menu menu = new Menu(almacen);
 	}
